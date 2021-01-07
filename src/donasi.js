feba80daec444827715399f6b423084cbdb5341a
LexusBot19 <77090280+LexusBot19@users.noreply.github.com>
2021-01-07 21:05:05 +0700

    Add files via upload

---
+++ src/donasi.js
@@ -0,0 +1,25 @@
+const donasi = () => {
+	return `
+
+┏━━━━━━━━━━━━━━━━━━━━
+┃          𝗗𝗢𝗡𝗔𝗦𝗜  
+┣━━━━━━━━━━━━━━━━━━━━
+┣━⊱ 𝗗𝗢𝗡𝗔𝗦𝗜 𝗕𝗢𝗦𝗤𝗨𝗘 ❉⊰━━✿
+┃  
+┣━⊱ *SAWERIA*
+┣⊱ https://saweria.co/donate/LexusCH
+┃
+┣━⊱ *TRAKTEER*
+┣⊱ https://trakteer.id/LexusCH
+┃
+┣━⊱ *PULSA*
+┣⊱ +62 821-2432-8193
+┃
+┣━━━━━━━━━━━━━━━━━━━━
+┃  *BOT BY AFFIS*
+┗━━━━━━━━━━━━━━━━━━━━
+
+`
+}
+
+exports.donasi = donasi
